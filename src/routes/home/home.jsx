bf4d55c012adb40be64ba5a4e3e02d1f5838ee94
Robert Westenberger <robert.michael.westenberger@gmail.com>
2019-12-01 19:23:32 -0700

    WIP install tinymce

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { Editor } from '@tinymce/tinymce-react';
 function Home() {
-    return (<h1>hello world!</h1>);
+    return (<Editor
+    apiKey="1jagv10p4zfc02pdtf2a328yth5exycy0swllhoe1ic4eeo5"
+    init={{
+        external_plugins: {
+            'tiny_mce_wiris' : 'http://localhost:3000/public/javascripts/plugin.min.js'
+        },
+        toolbar: 'tiny_mce_wiris_formulaEditor'
+    }}
+    />);
 }
 
 export default Home;
